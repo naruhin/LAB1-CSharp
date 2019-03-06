10aafdab68850628a5c7846a0468f3c802ab600b
naruhin <42322570+naruhin@users.noreply.github.com>
2019-03-06 16:41:50 +0200

    Добавьте файлы проекта.

---
+++ LAB1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB1
+{
+    class Program {
+
+        public static void squaringQuontientOfTwoRealNumbers(double fstNum,double sndNum)
+        {
+            double temp;
+            temp = fstNum / sndNum;
+            Console.WriteLine("({0} / {1})^2 = " + Math.Pow(temp, 2),fstNum,sndNum);
+
+        }
+        static void Main(string[] args)
+        {
+            double x, y;
+            Console.WriteLine("Введите два любых вещественных числа: ");
+            x = Convert.ToDouble(Console.ReadLine());
+            y = Convert.ToDouble(Console.ReadLine());
+            squaringQuontientOfTwoRealNumbers(x, y);
+            
+        }
+    }
+}
